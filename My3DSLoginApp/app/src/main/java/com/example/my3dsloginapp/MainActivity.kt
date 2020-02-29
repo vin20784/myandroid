3167d24926cbc8029d845534d653cb079933f145
vin20784 <vin20784@gmail.com>
2020-02-29 16:47:32 +0530

    android

---
+++ My3DSLoginApp/app/src/main/java/com/example/my3dsloginapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.my3dsloginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //companion object
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*var dObj = MyDataObjXfer(
+            Integer.valueOf(Id.text.toString()),Name.text.toString(),
+            Integer.parseInt(Age.text.toString())
+        )*/
+
+    }
+
+}
